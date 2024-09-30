4a3abd1f92e82bf4955a540d2d36e543a321b12d
Gunth15 <cjw041@email.latech.edu>
2024-09-30 15:07:06 -0500

    Database script and dummy data ready.

--- QDB/Dbscript.sql
+++ QDB/Dbscript.sql
@@ -1,35 +1,34 @@
-CREATE IF NOT EXISTS QuarrelDB; 
-
+CREATE DATABASE IF NOT EXISTS QuarrelDB; 
 USE QuarrelDB;
 
 # Follow_count has functional requirment to update when some gets a new follower
 CREATE TABLE IF NOT EXISTS User (
-  UID int NOT NULL AUTO_INCREMENT, 
-  Name varchar(255),
-  Username varchar(30),
-  Password varchar(30),
-  Follow_count INT,
-  PRIMARY KEY(UID)
+    UID int NOT NULL AUTO_INCREMENT, 
+    Name varchar(255),
+    Username varchar(30),
+    Password varchar(30),
+    Follow_count INT,
+    PRIMARY KEY(UID)
 );
 
 CREATE TABLE IF NOT EXISTS Follower (
-  Follows varchar(30),
-  Password varchar(30),
-  FOREIGN KEY(Follows) REFERENCES USER(UID),
-  FOREIGN KEY(Password) REFERENCES USER(UID)
+  Follows int,
+  Influencer int,
+  FOREIGN KEY(Follows) REFERENCES User(UID),
+  FOREIGN KEY(Influencer) REFERENCES User(UID)
 );
 
-# Like has functional requirment to update on liking post
 CREATE TABLE IF NOT EXISTS Arguments (
   AID int NOT NULL AUTO_INCREMENT,
   Comment varchar(280),
   Likes int,
   Views int,
-  Hyperlink varchar(500),
+  Hyperlink varchar(500) NULL,
   Poster int,
   T1_votes int,
   T2_votes int,
-  FOREIGN KEY(Poster) REFERENCES USER(UID)
+  PRIMARY KEY(AID),
+  FOREIGN KEY(Poster) REFERENCES User(UID)
 );
 
 CREATE TABLE IF NOT EXISTS Post (
@@ -38,29 +37,50 @@ CREATE TABLE IF NOT EXISTS Post (
   Likes int,
   Views int,
   Poster int,
-  FOREIGN KEY(Poster) REFERENCES USER(UID)
+  PRIMARY KEY(PID),
+  Hyperlink varchar(500) NULL,
+  FOREIGN KEY(Poster) REFERENCES User(UID)
 );
 
 CREATE TABLE IF NOT EXISTS Replies (
-  PID int NOT NULL AUTO_INCREMENT,
+  RID int NOT NULL AUTO_INCREMENT,
   Comment varchar(280),
   Likes int,
   Views int,
   Poster int,
-  Hyperlink varchar(500),
-  FOREIGN KEY(Poster) REFERENCES USER(UID)
+  PRIMARY KEY(RID),
+  FOREIGN KEY(Poster) REFERENCES User(UID)
+);
+
+CREATE TABLE IF NOT EXISTS Categories (
+  Category varchar(50),
+  tag varchar(20)
 );
 
 CREATE TABLE IF NOT EXISTS Arg_tags (
   arg int,
-  tag varchar(20)
+  tag varchar(20),
   FOREIGN KEY(arg) REFERENCES Arguments(AID)
-  FOREIGN KEY(tag) REFERENCES Categories(TAG)
 );
 
 CREATE TABLE IF NOT EXISTS Post_tags (
   post int,
-  tag varchar(20)
+  tag varchar(20),
   FOREIGN KEY(post) REFERENCES Post(PID)
-  FOREIGN KEY(tag) REFERENCES Categories(TAG)
 );
+
+ALTER TABLE Replies
+ADD column arg_reply int,
+ADD CONSTRAINT arg_reply
+FOREIGN KEY(arg_reply) REFERENCES Arguments(AID);
+
+ALTER TABLE Replies
+ADD column post_reply int,
+ADD CONSTRAINT post_reply
+FOREIGN KEY(post_reply) REFERENCES Post(PID);
+
+
+FLUSH PRIVILEGES;
+CREATE USER 'qapi'@'localhost' IDENTIFIED BY 'ARGS';
+GRANT ALL PRIVILEGES ON QuarrelDB.* TO 'qapi'@'localhost' IDENTIFIED BY 'ARGS';
+FLUSH PRIVILEGES;
