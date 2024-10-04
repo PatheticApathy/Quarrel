f5a2843978b58709da4efbb3e127845c7822caa8
Cheoddox <leeroywycoff@gmail.com>
2024-10-04 13:59:49 -0500

    Nav bar

--- Quarrel/src/App.vue
+++ Quarrel/src/App.vue
@@ -5,22 +5,94 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+    <div class="logo">
+      <span class="logospan">Quarrel</span>
+    </div>
+    
+    <div class="leftNavBarDiv1">
+      <nav class="leftNavBarNav" aria-label="Primary" role="Navigation">
+        <RouterLink to="/">
+          <div class="leftNavBarDiv2">
+            <div class="leftNavBarDiv3">
+              <svg class="leftNavBarSvg" view-box="0 0 24 24" aria-hidden="true">
+                <g>
+                  <path d="placeholder"></path>
+                </g>
+              </svg>
+            </div>
+            <div class="leftNavBarDiv4">
+              <span class="leftNavBarSpan" style="text-overflow:unset;">Home
+              </span>
+            </div>
+          </div>
+        </RouterLink>
+        <RouterLink to="/about">
+          <div class="leftNavBarDiv2">
+            <div class="leftNavBarDiv3">
+              <svg class="leftNavBarSvg" view-box="0 0 24 24" aria-hidden="true">
+                <g>
+                  <path d="placeholder"></path>
+                </g>
+              </svg>
+            </div>
+            <div class="leftNavBarDiv4">
+              <span class="leftNavBarSpan" style="text-overflow:unset;">About</span>
+            </div>
+          </div>
+        </RouterLink>
+        <RouterLink to="/explore">
+          <div class="leftNavBarDiv2">
+            <div class="leftNavBarDiv3">
+              <svg class="leftNavBarSvg" view-box="0 0 24 24" aria-hidden="true">
+                <g>
+                  <path d="placeholder"></path>
+                </g>
+              </svg>
+            </div>
+            <div class="leftNavBarDiv4">
+              <span class="leftNavBarSpan" style="text-overflow:unset;">Explore</span>
+            </div>
+          </div>
+        </RouterLink>
+        <RouterLink to="/messages">
+          <div class="leftNavBarDiv2">
+            <div class="leftNavBarDiv3">
+              <svg class="leftNavBarSvg" view-box="0 0 24 24" aria-hidden="true">
+                <g>
+                  <path d="placeholder"></path>
+                </g>
+              </svg>
+            </div>
+            <div class="leftNavBarDiv4">
+              <span class="leftNavBarSpan" style="text-overflow:unset;">Messages (Coming Soon)</span>
+            </div>
+          </div>
+        </RouterLink>
+        <RouterLink to="/profile">
+          <div class="leftNavBarDiv2">
+            <div class="leftNavBarDiv3">
+              <svg class="leftNavBarSvg" view-box="0 0 24 24" aria-hidden="true">
+                <g>
+                  <path d="placeholder"></path>
+                </g>
+              </svg>
+            </div>
+            <div class="leftNavBarDiv4">
+              <span class="leftNavBarSpan" style="text-overflow:unset;">Profile</span>
+            </div>
+          </div>
+        </RouterLink>
       </nav>
     </div>
+
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
+
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -38,22 +110,77 @@ nav {
   margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
   display: inline-block;
   padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
 }
 
-nav a:first-of-type {
-  border: 0;
+.logo {
+  position: absolute;
+  left: 10rem;
+  top: 0.5rem;
+  font-size: 3rem;
+  text-align: left;
+  color: navy;
+}
+
+.leftNavBarNav {
+  height: 552.75px;
+  width: 285px;
+  border-right: 1px solid var(--color-border);
+}
+
+.leftNavBarDiv1 {
+  height: 552.75px;
+  width: 259px;
+  position: absolute;
+  left: 12rem;
+  top: 5rem;
+}
+
+.leftNavBarA {
+  height: 50.25px;
+  width: 259px;
+}
+
+.leftNavBarDiv2 {
+  height: 50.25px;
+  width: 250px;
+  color: navy;
+}
+
+.leftNavBarDiv2:hover {
+  color: darkblue;
+}
+
+.leftNavBarDiv3 {
+  height: 26.25px;
+  width: 26.25px;
+}
+
+.leftNavBarSvg {
+  height: 26.25px;
+  width: 26.25px;
+}
+
+.leftNavBarDiv4 {
+  height: 55.1334px;
+  width: 200px;
+}
+
+.leftNavBarSpan {
+  height: 55.1334px;
+  width: 200px;
+}
+
+g {
+  height: 23px;
+  width: 21.8833px;
+}
+
+path {
+  height: 23px;
+  width: 21.8833px;
 }
 
 @media (min-width: 1024px) {
