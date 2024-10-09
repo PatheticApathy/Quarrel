25a4cbf345f5fbdd1d367543a0a8fc6ef7c909c2
ealberty <101431003+ealberty@users.noreply.github.com>
2024-10-09 14:24:12 -0500

    Sign up/log in pages

--- Quarrel/src/router/index.ts
+++ Quarrel/src/router/index.ts
@@ -5,17 +5,28 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/', 
+      redirect: '/welcome' 
+    },
+    {
+      path: '/welcome',
+      name: 'IntroView',
+      component: () => import('../views/IntroView.vue') 
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../components/LoginView.vue')
+    },
+    {
+      path: '/home',
       name: 'home',
-      component: HomeView
+      component: HomeView 
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue') 
     }
   ]
 })
