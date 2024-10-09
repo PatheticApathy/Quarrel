e2d622bf8e537bad1eb9d4326517379802e60724
Gunth15 <cjw041@email.latech.edu>
2024-10-08 19:18:34 -0500

    Revert "Added null check and usernames now have to be unique"
    This reverts commit a7ff3160ebda096d64f46423734b2d3bb4859995.

--- QDB/Dbscript.sql
+++ QDB/Dbscript.sql
@@ -5,8 +5,8 @@ USE QuarrelDB;
 CREATE TABLE IF NOT EXISTS User (
     UID int NOT NULL AUTO_INCREMENT, 
     Name varchar(255),
-    Username varchar(30) NOT NULL UNIQUE,
-    Password varchar(30) NOT NULL,
+    Username varchar(30),
+    Password varchar(30),
     Follow_count INT,
     PRIMARY KEY(UID)
 );
@@ -23,12 +23,12 @@ CREATE TABLE IF NOT EXISTS Follower (
 CREATE TABLE IF NOT EXISTS Arguments (
   AID int NOT NULL AUTO_INCREMENT,
   Comment varchar(280),
-  Likes int NOT NULL,
-  Views int NOT NULL,
+  Likes int,
+  Views int,
   Hyperlink varchar(500) NULL,
   Poster int,
-  T1_votes int NOT NULL,
-  T2_votes int NOT NULL,
+  T1_votes int,
+  T2_votes int,
   PRIMARY KEY(AID),
   FOREIGN KEY(Poster) REFERENCES User(UID)
 );
@@ -36,20 +36,20 @@ CREATE TABLE IF NOT EXISTS Arguments (
 CREATE TABLE IF NOT EXISTS Post (
   PID int NOT NULL AUTO_INCREMENT,
   Comment varchar(280),
-  Likes int NOT NULL,
-  Views int NOT NULL,
-  Poster int NOT NULL,
-  Hyperlink varchar(500) NULL,
+  Likes int,
+  Views int,
+  Poster int,
   PRIMARY KEY(PID),
+  Hyperlink varchar(500) NULL,
   FOREIGN KEY(Poster) REFERENCES User(UID)
 );
 
 CREATE TABLE IF NOT EXISTS Replies (
   RID int NOT NULL AUTO_INCREMENT,
   Comment varchar(280),
-  Likes int NOT NULL,
-  Views int NOT NULL,
-  Poster int NOT NULL,
+  Likes int,
+  Views int,
+  Poster int,
   PRIMARY KEY(RID),
   FOREIGN KEY(Poster) REFERENCES User(UID)
 );
