200cf29efff9597a77c4b2635c69792cea0bb4ef
Gunth15 <cjw041@email.latech.edu>
2024-10-12 09:59:59 -0500

    Final changes before THE CONVERGENCE

--- QAPI/app.ts
+++ QAPI/app.ts
@@ -1,26 +1,45 @@
-const mysql = require('mysql');
-const http = require('node:http');
+import { createPool, Pool } from 'mysql';
+import { user_routes, greeting_route } from './routes/routes';
+import { createServer, IncomingMessage, ServerResponse } from 'node:http'
 
+const port: String = '8081';
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'qapi',
-  password : 'ARGS',
-  database : 'QuarrelDB'
+const pool = createPool({
+  host: 'localhost',
+  user: 'qapi',
+  password: 'ARGS',
+  database: 'QuarrelDB'
 });
 
-connection.connect();
- 
- 
-const sever = http.createServer((req,res) => {
+function route_request(req: IncomingMessage, res: ServerResponse, mysql: Pool): void {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('access-control-allow-headers', 'Content-Type, Authorization');
+  if (req.url == '/') {
+    //TODO: Make greeting screen
+    greeting_route(req, res);
+    return
+  }
+  if (req.method == 'OPTIONS') {
+    res.writeHead(204)
+    res.end()
+  }
+  const route: string[] = req.url!.split('/');
+  switch (route[1]) {
+    case 'user':
+      //get user or add user
+      user_routes(req, res, route, mysql);
+      break
 
-    if(req.url == '/bob') {
-        console.log("been here")
-        res.writeHead(200,{'Content-Type':'application/json'})
-        res.end(JSON.stringify('bob'));
-    }
-    
+    default:
+      res.writeHead(404);
+      res.end("route does not exist yet");
+  }
+}
+
+const sever = createServer((req, res) => {
+  route_request(req, res, pool);
 });
 
-console.log("Running on localhost:8000");
-sever.listen('8000');
+console.log(`Running on localhost:${port}`);
+sever.listen(port);
