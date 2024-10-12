5ece5c31bb1d7b54e9c1049b7563165b97ef5a7c
ealberty <101431003+ealberty@users.noreply.github.com>
2024-10-12 16:07:47 -0500

    style changes complete/beginning of profile page

--- Qfrontend/src/views/ProfileView.vue
+++ Qfrontend/src/views/ProfileView.vue
@@ -3,6 +3,9 @@ import Navbar from './NavBarView.vue'
 </script>
 
 <template>
+    <div class="background-image">
+      <img src="../assets/background-image.jpg" width = 1000 height = 300>
+    </div>
     <div class="explore">
       <h1>This is a profile page</h1>
     </div>
@@ -10,6 +13,11 @@ import Navbar from './NavBarView.vue'
 </template>
 
 <style>
+.background-image {
+    position: absolute;
+    top: 0px;
+    left: 200px;
+}
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
