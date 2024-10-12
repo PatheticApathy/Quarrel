101dd90477c2bb8f34c21f8a2a222beff290b11b
ealberty <101431003+ealberty@users.noreply.github.com>
2024-10-12 17:25:29 -0500

    profile page continued...

--- Qfrontend/src/views/ProfileView.vue
+++ Qfrontend/src/views/ProfileView.vue
@@ -4,20 +4,118 @@ import Navbar from './NavBarView.vue'
 
 <template>
     <div class="background-image">
-      <img src="../assets/background-image.jpg" width = 1000 height = 300>
+      <img src="../assets/background-image.jpg" width = 1000 height = 250>
+    </div>
+    <div class="profile-pic">
+      <img src="../assets/profile-pic.jpg" width = 150 height = 150>
     </div>
     <div class="explore">
-      <h1>This is a profile page</h1>
+      <h1>Dr. Bowman</h1>
     </div>
+    <div class="bio">
+      <p>Finally a doctor!</p>
+      <p>Latech Computer Science</p>
+    </div>
+    <div class="minions">
+      <button>Minions: 12,414</button>
+      <button>Enemies: 0</button>
+    </div>
+    <button @click="go_to_edit_profile">Edit Profile</button>
     <Navbar />
 </template>
 
 <style>
+
 .background-image {
     position: absolute;
     top: 0px;
-    left: 200px;
+    left: 460px;
+    filter: blur(2px);
+    -webkit-filter: blur(2px);
 }
+
+.profile-pic {
+  position: absolute;
+  top: 175px;
+  left: 550px;
+  border-radius: 50%;
+  border: 5px solid black;
+  overflow: hidden;
+}
+
+h1 {
+  position: absolute;
+  top: 275px;
+  left: 730px;
+  font-family: 'Verdana', 'sans-serif';
+  font-weight: 900;
+}
+
+.bio {
+  display: block;
+  justify-content: space-between;
+  gap: 10px;
+  position: absolute;
+  top: 375px;
+  left: 575px;
+  font-size: 15px;
+  font-family: 'Verdana', 'sans-serif';
+  font-weight: 900;
+}
+
+.minions {
+  display: flex;
+  justify-content: space-between;
+  gap: 40px;
+  position: absolute;
+  top: 450px;
+  left: 575px;
+  font-size: 20px;
+  font-family: 'Verdana', 'sans-serif';
+  font-weight: 900;
+}
+
+.minions button {
+  border-radius: 40px;
+  background-color: navy;
+  padding: 20px;
+}
+
+  button {
+    position: absolute;
+    top: 300px;
+    left: 1050px;
+    width: 150px;
+    height: 150px;
+    border: 0px solid navy;
+    background-color: navy;
+    color: white;
+    cursor: pointer;
+    margin-top: 10px;
+    font-family: 'Verdana', 'sans-serif';
+    font-weight: 900;
+    font-size: 15px;
+    margin-top: 20px;
+    border-radius: 50%;
+  }
+
+  button:hover {
+        background-color: violet;
+        animation: bubble 1s ease-out;
+    }
+
+  @keyframes bubble {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.2);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
