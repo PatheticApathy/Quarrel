291cba6d533b1a685c5f7924ba1c30cfc7623110
Gunth15 <cjw041@email.latech.edu>
2024-10-16 15:34:25 -0500

    loign route now only returns UID

--- QAPI/handlers/users.ts
+++ QAPI/handlers/users.ts
@@ -1,7 +1,7 @@
 import { ServerResponse } from 'http';
 import '../models';
 import { Pool, Query } from 'mysql';
-import { insert_new_user, get_user_by_id, get_user_by_username } from '../sql/sql';
+import { insert_new_user, get_user_by_id, post_id_request } from '../sql/sql';
 
 //user based handlers
 export function add_user_handler(res: ServerResponse, user: User, sql: Pool): void {
@@ -32,13 +32,13 @@ export function get_id_handler(res: ServerResponse, id: number, sql: Pool): void
 
 
 export function get_user_by_username_handler(res: ServerResponse, login_info: Login, sql: Pool): void {
-  get_user_by_username(login_info, sql, (err, user) => {
+  post_id_request(login_info, sql, (err, id) => {
     if (err) {
       res.writeHead(500);
       res.end('Could not complete transaction/Invalid login');
     } else {
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(user));
+      res.end(JSON.stringify(id));
     }
   });
 }
