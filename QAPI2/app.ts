d890204161e11095be9a8c92fcf28d6c5fa41597
Gunth15 <cjw041@email.latech.edu>
2024-10-30 14:45:17 -0500

    Parsy body ahhhhhh

--- QAPI2/app.ts
+++ QAPI2/app.ts
@@ -3,6 +3,7 @@ import { createPool, Pool } from 'mysql';
 import { user_router } from './routes/routes';
 import { Express } from 'express';
 import express = require("express");
+import bodyParser = require('body-parser');
 
 const app: Express = express();
 const port: String = '8081';
@@ -14,6 +15,7 @@ const pool = createPool({
   database: 'QuarrelDB'
 });
 
+app.use(bodyParser.json());
 app.use("/", express.static("./greeting/index.html"));
 app.use('/user', user_router(pool));
 //app.use('/post', post_routes);
