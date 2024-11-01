c24ad1255399566d7547162254973d7cd19e61be
Gunth15 <cjw041@email.latech.edu>
2024-11-01 14:47:32 -0500

    Added search functionality

--- QAPI/routes/user.ts
+++ QAPI/routes/user.ts
@@ -1,4 +1,4 @@
-import { add_user_handler, get_id_handler, get_random_users_handler, post_login_request_handler } from '../handlers/handlers';
+import { add_user_handler, get_id_handler, get_random_users_handler, get_search_handler, post_login_request_handler } from '../handlers/handlers';
 import { Connection, Pool } from 'mysql';
 import { Router, Response, Request } from 'express';
 import express = require("express");
@@ -7,6 +7,7 @@ export function user_router(pool: Pool): Router {
   const router: Router = express.Router();
 
   router.get('/find/:id', (req: Request<{ id: number }>, res, next) => { get_id_handler(req, res, pool, next) });
+  router.get('/search/:search', (req: Request<{ search: string }>, res, next) => { get_search_handler(req, res, next, pool) });
   router.get('/batch', (req: Request, res, next) => { get_random_users_handler(req, res, next, pool) });
   router.post('/login', (req: Request<Login>, res: Response, next) => { post_login_request_handler(req, res, next, pool) })
   router.post('/signup', (req: Request<User>, res: Response, next) => { add_user_handler(req, res, next, pool) });
--- QAPI/sql/user.ts
+++ QAPI/sql/user.ts
@@ -57,14 +57,13 @@ export function drop_user_by_id(id: number, sql: Pool, callback: (err: Error | u
     callback(undefined, true);
   })
 }
-
 export function get_random_users(sql: Pool, callback: (err: Error | undefined, users: Array<User> | undefined) => void): void {
   sql.query('SELECT * FROM User ORDER BY RAND()+1 LIMIT 3;', function (error, result, _) {
     if (error) {
       console.error('Could not complete transaction:', error);
       callback(error, undefined);
     }
-    console.log(`User ${JSON.stringify(result)} retrieved`);
+    console.log(`Users ${JSON.stringify(result)} retrieved`);
     if (result.length == 0) {
       callback(undefined, undefined);
     } else {
@@ -72,3 +71,18 @@ export function get_random_users(sql: Pool, callback: (err: Error | undefined, u
     }
   })
 };
+export function regex_username(search_string: string, sql: Pool, callback: (err: Error | undefined, users: Array<User> | undefined) => void): void {
+  sql.query("SELECT * FROM User WHERE username REGEXP ?", `[A-Za-z]*${search_string}[A-Za-z]*`, function (error, result, _) {
+    if (error) {
+      console.error('Could not complete transaction:', error);
+      callback(error, undefined);
+    }
+    console.log(`Users ${JSON.stringify(result)} retrieved`);
+    if (result.length == 0) {
+      callback(undefined, undefined);
+    } else {
+      callback(undefined, result);
+    }
+  })
+};
+
