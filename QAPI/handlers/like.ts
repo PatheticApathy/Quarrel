e26fd57258fcf73a801ae28968b404f712a4943a
Gunth15 <cjw041@email.latech.edu>
2024-11-12 11:47:40 -0600

    Fixed bug that allowed users to add to like count even though they already liked

--- QAPI/handlers/like.ts
+++ QAPI/handlers/like.ts
@@ -21,7 +21,6 @@ export function get_likes_handler_p(req: Request<{ id: number }>, res: Response,
 
 export function get_liked_posts_handle_p(req: Request<{ UID: number, PIDs: Array<number> }>, res: Response, sql: Pool, next: NextFunction): void {
   const { UID, PIDs } = req.body;
-
   check_existing_like_post(UID, PIDs, sql, (err, liked_post) => {
     if (err) {
       next(err);
@@ -35,38 +34,38 @@ export function get_liked_posts_handle_p(req: Request<{ UID: number, PIDs: Array
 
 export function post_like_handler_p(req: Request<{ PID: number, UID: number }>, res: Response, sql: Pool, next: NextFunction): void {
   const { PID, UID } = req.body;
-  like_post(PID, sql, (err, liked) => {
+  record_user_like_post(UID, PID, sql, (err, like_transation_reciept) => {
     if (err) {
-      next(err)
-    } else if (!liked) {
-      res.status(500).json({ error: "Could not like post" });
+      next(err);
     } else {
-      record_user_like_post(UID, PID, sql, (err, like_transation_reciept) => {
+      like_post(PID, sql, (err, liked) => {
         if (err) {
-          next(err);
+          next(err)
+        } else if (!liked) {
+          res.status(500).json({ error: "Could not like post" });
         } else {
           res.status(200).json({ reciept: like_transation_reciept });
         }
-      })
+      });
     }
   });
-};
+}
 
 export function post_unlike_handler_p(req: Request<{ PID: number, UID: number }>, res: Response, sql: Pool, next: NextFunction): void {
   const { PID, UID } = req.body;
-  unlike_post(PID, sql, (err, unliked) => {
+  remove_user_like_post(UID, PID, sql, (err, complete_message) => {
     if (err) {
-      next(err)
-    } else if (!unliked) {
-      res.status(500).json({ error: "Could not unlike post" });
+      next(err);
     } else {
-      remove_user_like_post(UID, PID, sql, (err, complete_message) => {
+      unlike_post(PID, sql, (err, unliked) => {
         if (err) {
-          next(err);
+          next(err)
+        } else if (!unliked) {
+          res.status(500).json({ error: "Could not unlike post" });
         } else {
           res.status(200).json({ message: complete_message });
         }
-      })
+      });
     }
   });
-};
+}
